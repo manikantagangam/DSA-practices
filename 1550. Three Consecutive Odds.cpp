22ab17b4451979256a12024ee7d84b8838712278
manikantagangam <manikantagangam@gmail.com>
2023-11-18 11:06:27 +0530

    done 1550

---
+++ 1550. Three Consecutive Odds.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    bool threeConsecutiveOdds(vector<int> &arr)
+    {
+        int c = 0;
+        for (int a : arr)
+        {
+            if (a % 2 == 1)
+                c += 1;
+            else
+                c = 0;
+
+            if (c == 3)
+                return true;
+        }
+        return false;
+    }
+};
