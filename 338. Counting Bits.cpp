8f7ed904fadd087e1fb6065b5de5860bd5187290
manikantagangam <manikantagangam@gmail.com>
2023-11-24 21:42:33 +0530

    done 338

---
+++ 338. Counting Bits.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    vector<int> countBits(int n)
+    {
+        vector<int> ans(n + 1, 0);
+        int offset = 1;
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            if (offset * 2 == i)
+                offset = i;
+
+            ans[i] = 1 + ans[i - offset];
+        }
+
+        return ans;
+    }
+};
