2e802b69e2d38f62b330ff9a7403523703bea2c8
manikantagangam <manikantagangam@gmail.com>
2023-12-13 09:25:19 +0530

    done 1582

---
+++ 1582. Special Positions in a Binary Matrix.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    int numSpecial(vector<vector<int>> &mat)
+    {
+        int r = mat.size();
+        int c = mat[0].size();
+
+        vector<int> row(r);
+        vector<int> col(c);
+
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                if (mat[i][j] == 1)
+                {
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
