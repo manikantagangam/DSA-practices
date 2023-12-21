701ba8d2f4e659cbe026c6b8ec14dc7c7f10a4e5
manikantagangam <manikantagangam@gmail.com>
2023-12-21 19:05:34 +0530

    done 383

---
+++ 383. Ransom Note.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    bool canConstruct(string ransomNote, string magazine)
+    {
+        vector<int> c(26);
+        for (auto ch : magazine)
+            c[ch - 'a']++;
+
+        for (auto ch : ransomNote)
+        {
+            if (c[ch - 'a'] == 0)
+                return false;
+            c[ch - 'a']--;
+        }
+
+        return true;
+    }
+};
