0ee5c4199ae0ea76438b3fe6bebfe27e730721cf
manikantagangam <manikantagangam@gmail.com>
2024-08-17 23:58:51 +0530

    implemented dp tabulation fibonacci

---
+++ gfg-DSA-program/Dynamic Programming/Fibonacci-Numbers-Bottom-Up-DP.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    // Function to find the nth fibonacci number using bottom-up approach.
+    long long findNthFibonacci(int number)
+    {
+        // Your Code Here
+        vector<long long int> arr(number + 1);
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i < number + 1; i++)
+        {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[number];
+    }
+};
+
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+int main()
+{
+    int number;
+    cin >> number;
+    Solution obj;
+    cout << obj.findNthFjsonibonacci(number) << endl;
+    return 0;
+}
