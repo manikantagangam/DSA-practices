12d07d3f826b8957e2bdcb834b952269a7c7adfa
manikantagangam <manikantagangam@gmail.com>
2024-09-01 12:03:43 +0530

    modified

--- gfg-DSA-program/Sorting/sort_using_c++_stl.cpp
+++ gfg-DSA-program/Sorting/sort_using_c++_stl.cpp
@@ -20,24 +20,46 @@ using namespace std;
 //     }
 // }
 
-int main()
-{
-    vector<int> v;
+// int main()
+// {
+//     vector<int> v;
 
-    v = {10, 20, 5, 7};
+//     v = {10, 20, 5, 7};
 
-    sort(v.begin(), v.end());
+//     sort(v.begin(), v.end());
 
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout << v[i] << " ";
-    }
-    cout << endl;
+//     for (int i = 0; i < v.size(); i++)
+//     {
+//         cout << v[i] << " ";
+//     }
+//     cout << endl;
 
-    sort(v.begin(), v.end(), greater<int>());
+//     sort(v.begin(), v.end(), greater<int>());
 
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout << v[i] << " ";
+//     for (int i = 0; i < v.size(); i++)
+//     {
+//         cout << v[i] << " ";
+//     }
+// }
+
+struct Point{
+    int x, y;
+};
+
+bool myComp(Point p1, Point p2){
+    return p1.x < p2.x;
+}
+
+ostream& operator<<(ostream& os, const Point& p) {
+    os << "(" << p.x << ", " << p.y << ")";
+    return os;
+}
+
+int main(){
+    Point arr[] = {{3, 10}, {2, 8}, {5, 4}};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    sort(arr, arr+n, myComp);
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
     }
 }
