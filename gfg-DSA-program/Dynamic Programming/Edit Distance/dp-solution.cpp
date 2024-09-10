6367f7b821265c5c2052047f45b0ac1280c6e9b6
manikantagangam <manikantagangam@gmail.com>
2024-09-10 18:01:01 +0530

    Fix off-by-one error in edit distance calculation

--- gfg-DSA-program/Dynamic Programming/Edit Distance/dp-solution.cpp
+++ gfg-DSA-program/Dynamic Programming/Edit Distance/dp-solution.cpp
@@ -22,7 +22,7 @@ int eD(string s1, string s2, int m, int n){
     }
     for(int i = 1; i<m+1; i++){
         for(int j = 1; j<n+1; j++){
-            if(s1[i] == s2[j]){
+            if(s1[i-1] == s2[j-1]){
                 dp[i][j] = dp[i-1][j-1];
             }
             else{
