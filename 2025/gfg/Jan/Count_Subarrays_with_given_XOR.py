c84332d98032f705b4b640a9e16dc8130c7e9721
manikantagangam <manikantagangam@gmail.com>
2025-01-03 19:16:01 +0530

    implemented n^3 and n^2 solutions

---
+++ 2025/gfg/Jan/Count_Subarrays_with_given_XOR.py
@@ -0,0 +1,43 @@
+# # Brute Force Method 
+
+# O(n^3)
+
+# class Solution:
+#     def subarrayXor(self, arr, k):
+#         ans = []
+#         res = 0
+#         n = len(arr)
+
+#         # Function to compute XOR of a subarray
+#         def xor(l):
+#             x = l[0]
+#             for i in range(1, len(l)):
+#                 x = x ^ l[i]
+#             return x
+
+#         # Brute force approach to find all subarrays
+#         for i in range(n):
+#             for j in range(i, n):  # Start j from i to ensure valid subarrays
+#                 # Subarray arr[i:j+1]
+#                 if xor(arr[i:j+1]) == k:
+#                     ans.append(arr[i:j+1])
+#                     res += 1
+
+#         return res  # R
+
+
+## O(n^2)
+
+# class Solution:
+#     def subarrayXor(self, arr, k):
+#         # code here
+#         ans = 0
+#         for i in range(len(arr)):
+#             xor = 0
+#             for j in range(i, len(arr)):
+#                 xor = xor^arr[j]
+#                 if(xor == k):
+#                     ans+=1
+        
+#         return ans
+
